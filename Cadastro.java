6f85c43ba55e195594bcc4a9d1cd366145cea575
JoaoSantosSN <joao.p.santos280@aluno.senai.br>
2025-04-04 10:43:21 -0300

    Add files via upload

---
+++ Cadastro.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao cadastro de usuarios!");
+        
+        String studentName = "Joao Santos";
+        int studentID = 24;
+        int studentAge = 17;
+        float studentNote = 100.00f;
+        char studentGrade = 'B';
+    
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student note: " + studentNote);
+        System.out.println("Student grade:" + studentGrade);
+        
+    }
+    
+}
